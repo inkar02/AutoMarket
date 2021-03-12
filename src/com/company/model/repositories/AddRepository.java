e21c3d353bb208aa87673cfac3fa100daaf5277c
inkar02 <79473736+inkar02@users.noreply.github.com>
2021-03-13 00:07:25 +0600

    Update AddRepository.java

--- src/com/company/model/repositories/AddRepository.java
+++ src/com/company/model/repositories/AddRepository.java
@@ -1,42 +1,42 @@
-package com.company.model.repositories;
+package com.company.model.repositories;//package name
 
-import com.company.model.database.IDB;
-import com.company.model.entities.Car;
+import com.company.model.database.IDB;//Interface from database
+import com.company.model.entities.Car;//Import the Main entity
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.sql.Connection;//Connection
+import java.sql.PreparedStatement;//PreparedStatement
 import java.sql.ResultSet;
-import java.util.ArrayList;
+import java.util.ArrayList;//in order to use arrays
 
-public class AddRepository implements IAddRepository{
-    private final IDB db;
+public class AddRepository implements IAddRepository{ //class that implement Interface IAddRepository
+    private final IDB db; //db
 
-    public AddRepository(IDB db) {
+    public AddRepository(IDB db) { //constructor for db
         this.db = db;
     }
 
     @Override
-    public void addCar(Car newCar) {
-        Connection con = null;
+    public void addCar(Car newCar) { //function to add new cars
+        Connection con = null;//start
 
-        ArrayList< Car > res = new ArrayList<>();
+        ArrayList< Car > res = new ArrayList<>();//Array to show the list of cars
 
         try {
-            con = db.getConnection();
+            con = db.getConnection(); //connection
 
-            String sql = "insert into cars values (default, ?, ?, ?, ?, ?, ?, ?)";
+            String sql = "insert into cars values (default, ?, ?, ?, ?, ?, ?, ?)"; //input values to table
             PreparedStatement st = con.prepareStatement(sql);
 
-            st.setInt(1, newCar.getYear());
-            st.setString(2, newCar.getBrand());
-            st.setString(3, newCar.getModel());
-            st.setInt(4, newCar.getMileage());
-            st.setFloat(5, newCar.getVolume());
-            st.setInt(6, newCar.getPrice());
-            st.setString(7, newCar.getLocation());
+            st.setInt(1, newCar.getYear());//input year
+            st.setString(2, newCar.getBrand());//input brand
+            st.setString(3, newCar.getModel());//input model
+            st.setInt(4, newCar.getMileage());//input mileage
+            st.setFloat(5, newCar.getVolume());//input volume
+            st.setInt(6, newCar.getPrice());//input price
+            st.setString(7, newCar.getLocation());//place
 
             st.execute();
-        } catch (Exception e){
+        } catch (Exception e){ //trycatch function for addRepository(car list)
             System.out.println(e);
         } finally {
             try {
