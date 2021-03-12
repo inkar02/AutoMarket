f8be10918c91e2713aaaf2d951687ca9c61ba55b
inkar02 <79473736+inkar02@users.noreply.github.com>
2021-03-13 00:04:49 +0600

    Update SearchController.java

--- src/com/company/controller/SearchController.java
+++ src/com/company/controller/SearchController.java
@@ -1,23 +1,23 @@
-package com.company.controller;
+package com.company.controller; //package
 
-import com.company.model.entities.Car;
-import com.company.model.repositories.ISearchRepository;
+import com.company.model.entities.Car; //entity
+import com.company.model.repositories.ISearchRepository;//Interface of SearchRepository
 
-import java.util.ArrayList;
+import java.util.ArrayList; //to use array
 
-public class SearchController {
+public class SearchController { //class to search the cars
 
     private final ISearchRepository repo;
 
-    public SearchController(ISearchRepository repo) {
+    public SearchController(ISearchRepository repo) { //declare
         this.repo = repo;
     }
 
-    public ArrayList<Car> getAllCars(){
+    public ArrayList<Car> getAllCars(){ //used to show the list of all cars
         return repo.getAllCars();
     }
 
-    public ArrayList < Car > getCarsBySearch(Car search){
+    public ArrayList < Car > getCarsBySearch(Car search){ //array to search
         return repo.getCarsBySearch(search);
     }
 }
