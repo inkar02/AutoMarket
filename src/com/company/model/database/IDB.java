ddae5b29104fd9982c37222d63b2fab5305341e0
inkar02 <79473736+inkar02@users.noreply.github.com>
2021-03-13 00:05:22 +0600

    Update IDB.java

--- src/com/company/model/database/IDB.java
+++ src/com/company/model/database/IDB.java
@@ -1,9 +1,9 @@
-package com.company.model.database;
+package com.company.model.database; //interface to show the connection between Intellij and sql
 
-import java.sql.Connection;
+import java.sql.Connection; //import "must have" things
 import java.sql.SQLException;
 
 public interface IDB {
 
-    Connection getConnection() throws SQLException, ClassNotFoundException;
+    Connection getConnection() throws SQLException, ClassNotFoundException; //connection
 }
