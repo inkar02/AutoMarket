3e36fe744d8df70cd7a04fbbd4d8444985543a6b
inkar02 <79473736+inkar02@users.noreply.github.com>
2021-03-13 00:09:41 +0600

    Update Application.java

--- src/com/company/view/Application.java
+++ src/com/company/view/Application.java
@@ -1,5 +1,5 @@
-package com.company.view;
-
+package com.company.view; //package to show the main console application
+//import all other packages
 import com.company.controller.AddController;
 import com.company.controller.SearchController;
 import com.company.model.entities.Car;
@@ -9,39 +9,39 @@ import com.company.model.repositories.ISearchRepository;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Application {
+public class Application { //class about console app
 
-    private final AddController addController;
-    private final SearchController searchController;
+    private final AddController addController; //addcontroller
+    private final SearchController searchController;//searchcontroller
 
-    public Application(ISearchRepository searchRepo, IAddRepository addRepo){
+    public Application(ISearchRepository searchRepo, IAddRepository addRepo){ //application
         addController = new AddController(addRepo);
         searchController = new SearchController(searchRepo);
     }
 
 
-    public void start(){
+    public void start(){ //start app
         System.out.println("Hello, welcome to auto market!");
 
         Scanner in = new Scanner(System.in);
 
-        int choice;
+        int choice; //to choose commands
 
-        while(true){
+        while(true){ //main menu of project
             System.out.println("1: View cars");
             System.out.println("2: Add you car to our marketplace");
             System.out.println("0: Exit");
 
-            choice = in.nextInt();
+            choice = in.nextInt(); //continue to choose
 
-            if(choice == 1){
+            if(choice == 1){ //functions of 'view cars' part
                 System.out.println("1: Search cars");
                 System.out.println("2: Show all cars");
                 System.out.println("#: Return");
 
-                choice = in.nextInt();
+                choice = in.nextInt();//continue
 
-                if(choice == 1){
+                if(choice == 1){ //about search car
                     String brand = null, model = null;
                     Integer year = null;
                     Float volume = null;
@@ -65,7 +65,7 @@ public class Application {
                     continue;
                 }
 
-            } else if(choice == 2){
+            } else if(choice == 2){ //about add car
                 int year;
                 String brand, model, location;
                 int price, mileage;
@@ -96,7 +96,7 @@ public class Application {
 
     }
 
-    void searchCars(String brand, String model, int year, Float volume){
+    void searchCars(String brand, String model, int year, Float volume){ //function to show the info about cars
         ArrayList < Car > cars = searchController.getCarsBySearch(new Car(brand, model, year, volume));
         if(cars.size() == 0){
             System.out.println("Sorry, we don't have this car");
@@ -107,14 +107,14 @@ public class Application {
             System.out.println(car.getInfo());
         }
     }
-    void showAllCars(){
+    void showAllCars(){ //show list
         ArrayList < Car > cars = searchController.getAllCars();
         for(Car car : cars){
             System.out.println(car.getInfo());
         }
     }
 
-    void addCar(Car car){
+    void addCar(Car car){ //add car function and calling
         addController.addCar(car);
         System.out.println("----------------------- \n Success! We added your car to our marketplace \n ----------------------- \n");
     }
