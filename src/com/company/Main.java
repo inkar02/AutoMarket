dffb81e91c487a2ad21bbbac095815f7d1957c8f
inkar02 <79473736+inkar02@users.noreply.github.com>
2021-03-13 00:10:08 +0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,23 @@
-package com.company;
+package com.company; //package
 
-import com.company.model.database.IDB;
-import com.company.model.database.Postgres;
-import com.company.model.repositories.AddRepository;
-import com.company.model.repositories.IAddRepository;
-import com.company.model.repositories.ISearchRepository;
-import com.company.model.repositories.SearchRepository;
-import com.company.view.Application;
+import com.company.model.database.IDB; //connection
+import com.company.model.database.Postgres;//postgresql
+import com.company.model.repositories.AddRepository; //AddRepository
+import com.company.model.repositories.IAddRepository; //IAddRepository
+import com.company.model.repositories.ISearchRepository; //ISearchRepository
+import com.company.model.repositories.SearchRepository; //SearchRepository
+import com.company.view.Application; //Application
 
-public class Main {
+public class Main { //main
 
     public static void main(String[] args) {
-        IDB database = new Postgres();
-
+        IDB database = new Postgres(); //connection
+//get database info for both functions of console application
         ISearchRepository searchRepository = new SearchRepository(database);
         IAddRepository addRepository = new AddRepository(database);
 
-        Application app = new Application(searchRepository, addRepository);
+        Application app = new Application(searchRepository, addRepository);//functions of our application
 
-        app.start();
+        app.start(); //start to use this console app by calling this
     }
 }
