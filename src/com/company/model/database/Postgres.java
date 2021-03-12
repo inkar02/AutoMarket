7f81392ec397ee2b14f18eec3181f8419ae7cbb6
inkar02 <79473736+inkar02@users.noreply.github.com>
2021-03-13 00:05:50 +0600

    Update Postgres.java

--- src/com/company/model/database/Postgres.java
+++ src/com/company/model/database/Postgres.java
@@ -1,19 +1,18 @@
-package com.company.model.database;
+package com.company.model.database; //package about the connection with db
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.Connection; //Connection
+import java.sql.DriverManager; //DriverManager
+import java.sql.SQLException; //Exception
 
-public class Postgres implements IDB {
+public class Postgres implements IDB { //connection with db by using interface
     @Override
     public Connection getConnection() throws SQLException, ClassNotFoundException {
-        String connectionURL = "jdbc:postgresql://localhost:5432/inkarrrrrrrr";
+        String connectionURL = "jdbc:postgresql://localhost:5432/inkarrrrrrrr"; //name of database on postgresql
         try {
-            Class.forName("org.postgresql.Driver");
-            return DriverManager.getConnection(connectionURL, "postgres", "inkar2002");
+            Class.forName("org.postgresql.Driver"); //driver
+            return DriverManager.getConnection(connectionURL, "postgres", "inkar2002"); //info about user
         } catch (Exception e) {
             System.out.println(e.getMessage());
             throw e;
         }
     }
-}
